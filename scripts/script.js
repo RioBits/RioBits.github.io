d8ff422eeb3e5732cf5f36c4438536a677895c75
RioBits <abudejesry1@gmail.com>
2020-09-05 21:14:43 +0300

    Dark mode & Some New Styles

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+const lightmode = document.getElementById('light');
+const darkmode = document.getElementById('dark');
+light.addEventListener('click', () => {
+    changeDisplay("none", "block", "#613dff", "aliceblue", "black")
+})
+
+dark.addEventListener('click', () => {
+    changeDisplay("block", "none", "black", "#151519", "aliceblue")
+})
+
+function changeDisplay(light, dark, headerbg, sec1bg, sec1color) {
+    const header = document.getElementsByTagName('header')[0];
+    const sec1 = document.getElementById('sec-1');
+    header.style.backgroundColor = headerbg;
+    sec1.style.backgroundColor = sec1bg;
+    sec1.style.color = sec1color;
+    lightmode.style.display = light;
+    darkmode.style.display = dark;
+}
