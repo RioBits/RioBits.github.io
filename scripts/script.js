fa913ebc8369c1357506200d62ff1ec8ac771060
RioBits <abudejesry1@gmail.com>
2020-09-14 21:59:20 +0300

    some edits

--- scripts/script.js
+++ scripts/script.js
@@ -11,9 +11,11 @@ dark.addEventListener('click', () => {
 function changeDisplay(light, dark, headerbg, sec1bg, sec1color) {
     const header = document.getElementsByTagName('header')[0];
     const sec1 = document.getElementById('sec-1');
+    const body = document.getElementsByTagName('body')[0]
     header.style.backgroundColor = headerbg;
     sec1.style.backgroundColor = sec1bg;
     sec1.style.color = sec1color;
     lightmode.style.display = light;
     darkmode.style.display = dark;
+    body.style.backgroundColor = sec1bg;
 }
